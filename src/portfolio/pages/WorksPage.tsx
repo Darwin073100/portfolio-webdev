1e6af091b3cc05fe47eafe6339fd30be7f23f5f4
Edwin Garcia Quiterio <edwinegq@outlook.com>
2024-12-17 08:56:45 -0600

    second part responsive

--- src/portfolio/pages/WorksPage.tsx
+++ src/portfolio/pages/WorksPage.tsx
@@ -12,25 +12,47 @@ export const WorksPage = () => {
         title='Works' 
         text='El conocimiento es poder, nunca paremos de aprender.'/>
         <div className="works-page">
-          <div className="project-name">
-          <h2><TagElement tag="Nombre del proyecto"/></h2>
-          <NavLink className='link-work-url-code' to={'/'}><i className="fa fa-github"></i></NavLink>
-          <NavLink className='link-work-url-deploy' to={'/'}><i className="fa fa-television"></i></NavLink>
+          <div className="project-item">
+            <div className="project-name">
+            <h2><TagElement tag="Nombre del proyecto"/></h2>
+            <NavLink className='link-work-url-code' to={'/'}><i className="fa fa-github"></i></NavLink>
+            <NavLink className='link-work-url-deploy' to={'/'}><i className="fa fa-television"></i></NavLink>
+            </div>
+            <div className="projects-details">
+                <img src={ imgSrc } alt="project" />
+            </div>
+            <div className="project-description">
+            <TagElement tag='<p>'/>
+              <br />
+              <h3 className='one-tab'><TagElement tag='Descripción del proyecto'/></h3>
+              <p className='one-tab'>Soy una persona que le gusta los retos, quiero aprender día a día sobre lo que me apasiona, 
+              soy estudiante, comprometido con mi carrera y mi sabiduría. Me gusta ser responsable con mis 
+              tareas, trato de cumplir al <TagElement tag='100 %'/> lo que me corresponde, la responsabilidad es un aspecto muy 
+              importante para mí.</p>
+            <TagElement tag='<p>'/>
+            <div className="bar"></div>
+            </div>
           </div>
-          <div className="projects-details">
-            <button className="float-menu-item"><i className="fa fa-angle-left" aria-hidden="true"></i></button>
-              <img src={ imgSrc } alt="project" />
-            <button className="float-menu-item"><i className="fa fa-angle-right" aria-hidden="true"></i></button>
-          </div>
-          <div className="project-description">
-          <TagElement tag='<p>'/>
-            <br />
-            <h3 className='one-tab'><TagElement tag='Descripción del proyecto'/></h3>
-            <p className='one-tab'>Soy una persona que le gusta los retos, quiero aprender día a día sobre lo que me apasiona, 
-            soy estudiante, comprometido con mi carrera y mi sabiduría. Me gusta ser responsable con mis 
-            tareas, trato de cumplir al <TagElement tag='100 %'/> lo que me corresponde, la responsabilidad es un aspecto muy 
-            importante para mí.</p>
-          <TagElement tag='<p>'/>
+          <div className="project-item">
+            <div className="project-name">
+            <h2><TagElement tag="Nombre del proyecto"/></h2>
+            <NavLink className='link-work-url-code' to={'/'}><i className="fa fa-github"></i></NavLink>
+            <NavLink className='link-work-url-deploy' to={'/'}><i className="fa fa-television"></i></NavLink>
+            </div>
+            <div className="projects-details">
+                <img src={ imgSrc } alt="project" />
+            </div>
+            <div className="project-description">
+            <TagElement tag='<p>'/>
+              <br />
+              <h3 className='one-tab'><TagElement tag='Descripción del proyecto'/></h3>
+              <p className='one-tab'>Soy una persona que le gusta los retos, quiero aprender día a día sobre lo que me apasiona, 
+              soy estudiante, comprometido con mi carrera y mi sabiduría. Me gusta ser responsable con mis 
+              tareas, trato de cumplir al <TagElement tag='100 %'/> lo que me corresponde, la responsabilidad es un aspecto muy 
+              importante para mí.</p>
+            <TagElement tag='<p>'/>
+            <div className="bar"></div>
+            </div>
           </div>
         </div>
     </main>
